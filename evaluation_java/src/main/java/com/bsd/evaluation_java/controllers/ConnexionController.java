5bcfa986a977df480113b822dcd9717e4fdb8605
Pi-et-Rho <pierre.michel.work@gmail.com>
2024-12-13 14:56:14 +0100

    Ajout Connexion (Controllers)

---
+++ evaluation_java/src/main/java/com/bsd/evaluation_java/controllers/ConnexionController.java
@@ -0,0 +1,46 @@
+package com.bsd.evaluation_java.controllers;
+
+import com.bsd.evaluation_java.models.Utilisateur;
+import com.bsd.evaluation_java.security.AppUserDetails;
+import com.bsd.evaluation_java.security.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class ConnexionController {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    AuthenticationProvider authenticationProvider;
+
+    @PostMapping("/connexion")
+    public ResponseEntity<String> connexion(@RequestBody Utilisateur utilisateur) {
+
+        try {
+            AppUserDetails appUserDetails = (AppUserDetails) authenticationProvider
+                    .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                    utilisateur.getEmail(),
+                                    utilisateur.getPassword()))
+                    .getPrincipal();
+
+            return ResponseEntity.ok(jwtUtils.generationToken(appUserDetails.getUsername()));
+
+        } catch (AuthenticationException ex) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @GetMapping("/test-jwt")
+    public String testJwt() {
+        return jwtUtils.generationToken("a@a.com");
+    }
+}
