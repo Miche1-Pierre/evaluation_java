fc60dd9a543d4fba8a03ccc36c32175b53d66a61
Pi-et-Rho <pierre.michel.work@gmail.com>
2024-12-13 16:41:22 +0100

    Modification AppUserDetails

--- evaluation_java/src/main/java/com/bsd/evaluation_java/security/AppUserDetails.java
+++ evaluation_java/src/main/java/com/bsd/evaluation_java/security/AppUserDetails.java
@@ -3,12 +3,9 @@ package com.bsd.evaluation_java.security;
 import com.bsd.evaluation_java.models.Utilisateur;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 
 public class AppUserDetails implements UserDetails {
 
